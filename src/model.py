43976bdf5a8a56d6cebc32cd3b970fa25b965de5
Landon Thomas <landon7@clemson.edu>
2026-06-16 16:09:31 -0400

    made

--- src/model.py
+++ src/model.py
@@ -7,7 +7,7 @@ def build_model(input_dim):
     #create the three layers of the neural network and output a single number, the percent
     #that the model believes the answer is 'yes'
     model = tf.keras.Sequential([
-        tf.keras.layers.Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
+        tf.keras.layers.Dense(64, activation='relu', input_shape=(input_dim,)),
         tf.keras.layers.Dense(32, activation='relu'),
         tf.keras.layers.Dense(1, activation='sigmoid')
     ])
