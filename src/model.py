7f7ecda2cd76492f3715300bf528e5de3a415d9b
Landon Thomas <landon7@clemson.edu>
2026-06-16 15:59:56 -0400

    edited model.py

--- src/model.py
+++ src/model.py
@@ -2,22 +2,26 @@ import tensorflow as tf
 from sklearn.metrics import classification_report, confusion_matrix
 #This file will contain the structuring and build of the Neural Network
 
-#create the three layers of the neural network and output a single number, the percent
-#that the model believes the answer is 'yes'
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
-    tf.keras.layers.Dense(32, activation='relu'),
-    tf.keras.layers.Dense(1, activation='sigmoid')
-])
+def build_model(input_dim):
 
-#compile the model
-model.compile(
-    optimizer='adam',
-    loss='binary_crossentropy',
-    metrics=['accuracy']
-)
+    #create the three layers of the neural network and output a single number, the percent
+    #that the model believes the answer is 'yes'
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
+        tf.keras.layers.Dense(32, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
 
-#then train the model on the data
+    #compile the model
+    model.compile(
+        optimizer='adam',
+        loss='binary_crossentropy',
+        metrics=['accuracy']
+    )
+
+    return model
+
+"""#then train the model on the data
 history = model.fit(
     X_train, y_train,
     epochs=10,
@@ -29,7 +33,7 @@ history = model.fit(
 y_pred = (model.predict(X_test) > 0.5).astype("int32")
 
 print(classification_report(y_test, y_pred))
-print(confusion_matrix(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))"""
 
 
 
