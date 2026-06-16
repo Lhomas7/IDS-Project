e3abaa6cd03d98e9f1db10febde00029862ac4d8
Landon Thomas <landon7@clemson.edu>
2026-06-16 16:04:58 -0400

    created main file to run program

---
+++ main.py
@@ -0,0 +1,29 @@
+from src.preprocess import load_and_preprocess
+from src.model import build_model
+from src.train import train_model
+from src.evaluate import evaluate_model
+from src.alerts import generate_alert
+
+def main():
+    # Load and preprocess data
+    X_train, X_test, y_train, y_test = load_and_preprocess("data/KDDTrain+.txt")
+
+    # Build model
+    model = build_model(X_train.shape[1])
+
+    # Train model
+    history = train_model(model, X_train, y_train)
+
+    # Evaluate model
+    y_pred = evaluate_model(model, X_test, y_test)
+
+    # Generate alerts for first 10 samples
+    print("\n--- Sample Alerts ---")
+    probs = model.predict(X_test[:10])
+
+    for p in probs:
+        pred = int(p > 0.5)
+        generate_alert(pred, float(p))
+
+if __name__ == "__main__":
+    main()
