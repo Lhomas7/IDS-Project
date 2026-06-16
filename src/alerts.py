909d64b20cfebe92ca4b928a04b3e5494d01c810
Landon Thomas <landon7@clemson.edu>
2026-06-16 16:02:54 -0400

    created alerts

--- src/alerts.py
+++ src/alerts.py
@@ -5,10 +5,3 @@ def generate_alert(prediction, confidence):
         print(f"[ALERT] Malicious traffic detected | Confidence: {confidence:.2f}")
     else:
         print("[INFO] Normal traffic")
-
-#run and test 
-probs = model.predict(X_test[:10])
-
-for p in probs:
-    pred = int(p > 0.5)
-    generate_alert(pred, float(p))
